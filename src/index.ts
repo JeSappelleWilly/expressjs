e65ab8caa20fa38822fb1105d51b8a1b9d0fd977
JeSappelleWilly <wserge98@gmail.com>
2025-04-04 14:45:46 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,48 @@ const app = express();
 const port = process.env.PORT || 3333;
 
 app.use(express.json());
-app.use(express.raw({ type: "application/vnd.custom-type" }));
-app.use(express.text({ type: "text/html" }));
 
-app.get("/", async (req, res) => {
-  res.json({ body: "Hello world!" });
+
+const processedMessages = new Map(); // Store processed message IDs with timestamps
+
+// Constants
+const WHATSAPP_API_VERSION = "v22.0";
+const PHONE_NUMBER_ID = process.env.PHONE_NUMBER_ID || "574770619057271";
+const WHATSAPP_API_URL = `https://graph.facebook.com/${WHATSAPP_API_VERSION}/${PHONE_NUMBER_ID}/messages`;
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN || "ceSecret";
+const DEFAULT_RECIPIENT = process.env.DEFAULT_RECIPIENT || "22656284997";
+
+const WHATSAPP_TOKEN = process.env.WHATSAPP_TOKEN ||
+
+
+// Webhook verification endpoint
+app.get("/*", (req, res) => {
+  // Parse the query params
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  console.log("-------------- New Request GET --------------");
+  console.log("Headers:", JSON.stringify(req.headers, null, 3));
+  console.log("Body:", JSON.stringify(req.body, null, 3));
+
+  // Check if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Check the mode and token sent is correct
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
+      // Respond with the challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+
+      res.status(200).send(challenge);
+    } else {
+      console.log("Responding with 403 Forbidden");
+      // Respond with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  } else {
+    console.log("Replying Thank you.");
+    res.json({ message: "Thank you for the message" });
+  }
 });
 
 app.listen(port, () => {
